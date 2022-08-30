c07130e4cb14862e4085c1c814e3861961eee3af
Geekman_1337 <33376894+Maomaonihao@users.noreply.github.com>
2022-08-30 16:13:27 +0800

    Update GlobalDefine.hpp

--- offline/include/GlobalDefine.hpp
+++ offline/include/GlobalDefine.hpp
@@ -1,10 +1,3 @@
- ///
- /// @file    GlobalDefine.hpp
- /// @author  lemon(haohb13@gmail.com)
- /// @date    2016-01-18 15:03:02
- ///
-
-
 #ifndef _WD_GLOBAL_DEFINE_H_
 #define _WD_GLOBAL_DEFINE_H_
 
